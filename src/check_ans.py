95c023da2badcc8c27e7d4fb89e2014b7dbcf2c6
justin.hsu <justin.hsu.1019@gmail.com>
2024-10-07 17:35:20 +0800

    feat: update alpha=0.0

--- src/check_ans.py
+++ src/check_ans.py
@@ -14,15 +14,15 @@ def clean_text(text):
 
 def calculate_accuracy(file_path):
     df = pd.read_excel(file_path)
-    total_questions = len(df) // 10
-    accuracies = {round(x * 0.1, 1): 0 for x in range(10, 0, -1)}
+    total_questions = len(df) // 11
+    accuracies = {round(x * 0.1, 1): 0 for x in range(10, -1, -1)}
 
     for i in range(total_questions):
-        correct_answer = clean_text(df.iloc[i * 10]["答案"])
+        correct_answer = clean_text(df.iloc[i * 11]["答案"])
         for alpha_idx, alpha in enumerate(
-            [round(x * 0.1, 1) for x in range(10, 0, -1)]
+            [round(x * 0.1, 1) for x in range(10, -1, -1)]
         ):
-            results_str = df.iloc[i * 10 + alpha_idx][f"檢索結果_{alpha}"]
+            results_str = df.iloc[i * 11 + alpha_idx][f"檢索結果_{alpha}"]
             results_list = ast.literal_eval(results_str)
             results_cleaned = [clean_text(result) for result in results_list]
             if correct_answer in results_cleaned:
